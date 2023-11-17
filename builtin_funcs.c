bddec37aed2b5dd7b0fdd1c639a7687b33268361
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-17 01:29:18 +0000

    after the test suite

--- builtin_funcs.c
+++ builtin_funcs.c
@@ -86,7 +86,7 @@ void pall(stack_t **stack, unsigned int line_number)
 		current = current->next;
 	while (current)
 	{
-		printf("%d\n", current->n);
+		fprintf(stdout, "%d\n", current->n);
 		current = current->prev;
 	}
 }
